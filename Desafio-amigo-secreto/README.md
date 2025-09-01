6ab9e014632ff61e6746bc0afb6c4c831f414c48
SoyDuarte <djavierd-05@hotmail.com.ar>
2025-09-01 12:19:53 -0300

    cambio readme y js

--- Desafio-amigo-secreto/README.md
+++ Desafio-amigo-secreto/README.md
@@ -1,2 +1,4 @@
 # Desafio-amigo-secreto
 Challage de Alura Latam G9
+
+Ingresa y sortea nombres en el input, la consola enlistara los nombres y no permitira repeticiones.
--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//el principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el probl
+
+let amigos = [];
+
+      function agregarAmigo() {
+      let input = document.getElementById("amigo");
+      let nombre = input.value.trim();
+
+      if (nombre === "") {
+        alert("Por favor escribe un nombre válido");
+        return;
+      }
+      
+      if (amigos.includes(nombre)) {
+        alert("Ese nombre ya esta en la lista");
+        input.value = "";
+        input.focus();
+        return
+      }
+      
+      amigos.push(nombre);
+      mostrarAmigos();
+
+      input.value = "";
+      input.focus();
+
+    }
+
+    function mostrarAmigos() {
+      let ul = document.getElementById("listaAmigos");
+      listaAmigos.innerHTML = "";
+      for (let i = 0; i < amigos.length; i++) {
+        li = document.createElement("lista");
+        li.textContent = amigo[i];
+        ul.appendChild(li);
+      }
+      console.log("Lista de amigos", amigos);
+    }
+
+    
+
+    function sortearAmigo() {
+        if (amigos.length === 0) {
+        alert("La lista de amigos esta vacia");
+        return;
+    }
+
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[indice];
+
+    document.getElementById("resultado").innerHTML =
+    `Tu amigo secreto es: ${amigoSecreto}`
+}
